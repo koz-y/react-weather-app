766fee21113cb61db5be44835bc9347bf8586a60
koz-y <77233172+koz-y@users.noreply.github.com>
2022-03-09 16:47:47 +0900

    AppからResultsへprops check

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,38 @@ import Results from "./components/Results";
 
 import "./App.css";
 
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperture: string;
+  conditionText: string;
+  icon: string;
+};
+
 function App() {
   const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperture: "",
+    conditionText: "",
+    icon: "",
+  });
   const getWeather = (event: any) => {
     event.preventDefault();
     fetch(
       "https://api.weatherapi.com/v1/current.json?key=e45f4be218cb49259cf125320222201&q=London&aqi=no"
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setResults({
+          country: data.location.country,
+          cityName: data.location.name,
+          temperture: data.current.temp_c,
+          conditionText: data.current.condition.text,
+          icon: data.current.condition.icon,
+        });
+      });
   };
 
   return (
@@ -22,7 +45,7 @@ function App() {
       <div>
         <Title></Title>
         <Form setCity={setCity} getWeather={getWeather}></Form>
-        <Results></Results>
+        <Results results={results}></Results>
       </div>
     </div>
   );
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,7 +1,18 @@
-const Results = () => {
+type ResultsPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperture: string;
+    conditionText: string;
+    icon: string;
+  };
+};
+
+const Results = (props: ResultsPropsType) => {
   return (
     <div>
       <h1>Resultページ</h1>
+      <p>{props.results.cityName}</p>
     </div>
   );
 };
